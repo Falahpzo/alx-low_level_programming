2fd7b253fd1f1d74241fe085abce9a4b8e230a94
Ekom <Dreeps001@gmail.com>
2023-06-16 19:38:21 -0700

    if

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: Prints the character 'A' followed by a new line
+ * Description: Prints the lowercase alphabet followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('A');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
 	return (0);
 }

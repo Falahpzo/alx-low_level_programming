0837522be59299b213210db5fe6664b5566f07bc
Ekom <Dreeps001@gmail.com>
2023-06-16 19:23:16 -0700

    if

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Checks the last digit of a randomly generated number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}

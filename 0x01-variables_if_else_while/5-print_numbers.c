e037172c42af828481fb2483eef0b1ace67475de
Ekom <Dreeps001@gmail.com>
2023-06-17 03:55:44 -0700

    if

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,18 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single digit numbers of base 10 starting from 0,
- *              followed by a new line
+ * Description: 'Prints numbers of base 10'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number;
+	int n;
 
-	for (number = 0; number < 10; number++)
-		putchar(number + '0');
-
-	putchar('\n');
-
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
 	return (0);
 }

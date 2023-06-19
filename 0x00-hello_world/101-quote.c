6005fff382fd2d7e245826664e7bb2df9e4be8e5
Ekom <Dreeps001@gmail.com>
2023-06-19 01:53:50 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const int len = 59;
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const int len = 59;
 
-    write(2, str, len);
+	write(2, str, len);
 
-    return (1);
+	return (1);
 }

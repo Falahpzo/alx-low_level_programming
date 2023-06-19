4e48f286f610cfcab0c123305b631687f4957910
Ekom <Dreeps001@gmail.com>
2023-06-19 01:51:36 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 1
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const int len = 59;
+    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const int len = 59;
 
-	write(2, str, len);
-	return (1);
+    write(2, str, len);
+
+    return (1);
 }

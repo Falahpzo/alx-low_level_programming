4d34195e7ec2e97cd1dbd2806d60bf9a614a6f61
Ekom <Dreeps001@gmail.com>
2023-06-21 08:24:48 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,18 @@
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase ten times followed by a new line
  *
+ * Description: This function prints the alphabet in lowercase ten times,
+ *              each on a separate line.
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (j = 1; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 97; j <= 112; j++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}

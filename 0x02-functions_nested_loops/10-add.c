49ee5d36201fe67be632bebec20ee0873a00a354
Ekom <Dreeps001@gmail.com>
2023-06-22 08:44:32 -0700

    nested

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,12 @@
 
 /**
  * add - Adds two values
- * @n1: The first number to be computed
- * @n2; The second number to be computed
+ * @a: The first number to be computed
+ * @b: The second number to be computed
  *
- * Return: Integer value
+ * Return: The sum of a and b
  */
-
 int add(int a, int b)
 {
-	return(a + b);
-
+	return (a + b);
 }

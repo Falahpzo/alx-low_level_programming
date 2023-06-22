de11ccee2e06ac93b6705393a3950d8fd0591245
Ekom <Dreeps001@gmail.com>
2023-06-22 09:13:27 -0700

    nested

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,16 @@
  */
 void print_to_98(int n)
 {
-	int i;
+    if (n >= 98)
+    {
+        for (; n > 98; n--)
+            printf("%d, ", n);
+    }
+    else
+    {
+        for (; n < 98; n++)
+            printf("%d, ", n);
+    }
 
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf(%d, ", n--);
-		printf("%d\n", n);
-	}
-	else
-	{
-		while (n < 98)
-			printf("%d", ", n++);
-		printf("%d\n", n);
-	}
+    printf("%d\n", n);
 }

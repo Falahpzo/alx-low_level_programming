7710aa76b94ed73f3583c5cd9e3e006e2a938bf4
Ekom <Dreeps001@gmail.com>
2023-07-13 16:00:59 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib>
+#include <stdio.h>
+
+int find_len(char *str);
+char *create_parray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prd, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char n*next, int next_length
